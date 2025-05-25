53d2334bc6c555e98d93efa87b63922dc050f7f9
ryotakonishi <ryota.konishi@icloud.com>
2025-05-25 16:39:18 +0900

    初コミ

---
+++ Assets/Scripts/PinBallBase/BallDeadZone.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDeadZone : MonoBehaviour
+{
+    private MoneyManager _moneyManager;
+    //TODO:値は決まってないから仮置き中
+    [SerializeField] int _addMoneyOnBallDrop = 100;//ボールが落下した時に手に入るお金
+    private void Awake()
+    {
+        _moneyManager = GameObject.FindAnyObjectByType<MoneyManager>();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        //ボールがあたり判定に入ったら消す
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Destroy(other.gameObject);
+            _moneyManager.AddMoney(_addMoneyOnBallDrop);
+        }
+    }
+}
